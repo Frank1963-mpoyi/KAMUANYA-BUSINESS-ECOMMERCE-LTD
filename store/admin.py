aa6f787abd58a0a9d91ab52f4709698f78affcd4
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2021-10-04 08:04:25 +0200

    working with unautheneticated user

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import OrderItem, Product, Order
+from .models import OrderItem, Product, Order, ShippingAddress
 # Register your models here.
 
 
-admin.site.register([OrderItem, Product, Order])
+admin.site.register([OrderItem, Product, Order, ShippingAddress])
--- store/views.py
+++ store/views.py
@@ -48,9 +48,40 @@ def cart(request):
         #print(items)
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart ={}
+
+        #print(f"CART: {cart}")
         items = []
         order = {'get_cart_total':0, 'get_cart_items': 0, 'sipping':False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]["quantity"]
+            #print('yooooo',cartItems)
+
+            product = Product.objects.get(id=i)
+
+            total = (product.price * cart[i]["quantity"])
+
+            order['get_cart_total'] += total
+            order['get_cart_items']+=  cart[i]["quantity"]
+
+            item = {
+
+                'product':{
+                    'id': product.id,
+                    'title': product.title,
+                    'price': product.price,
+                    'image_url': product.image_url
+                },
+                'quantity': cart[i]['quantity'],
+                'get_total':total,
+            }
+
+            items.append(item)
 
     context = {'items': items, 'order': order, 'cartItems':cartItems}
 
